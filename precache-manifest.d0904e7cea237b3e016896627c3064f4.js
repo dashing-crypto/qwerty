736d8a64fe6a7b7877850fe47aa8c9ff1c5aa315
Your Name <you@example.com>
2020-10-19 14:02:48 +0530

    Updates

---
+++ precache-manifest.d0904e7cea237b3e016896627c3064f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71db5d936715cf287c7fb8c8563bf7d8",
+    "url": "/qwerty/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/qwerty/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "27ebaec51684958f1aff",
+    "url": "/qwerty/static/css/main.b1e936cb.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/qwerty/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/qwerty/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ebaec51684958f1aff",
+    "url": "/qwerty/static/js/main.62e24ee4.chunk.js"
+  },
+  {
+    "revision": "d8d1625a1e73efac07d4",
+    "url": "/qwerty/static/js/runtime-main.6cf92b4f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/qwerty/static/media/SEGA.12a3db8b.woff"
+  }
+]);
